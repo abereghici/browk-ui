50e829c48888e295da186d99e508253dd0689a4e
Alexandru Bereghici <abereghici@tripadvisor.com>
2021-11-11 18:03:42 +0200

    chore: upgraded yarn to 3.1.0. added babel config

---
+++ babel.config.js
@@ -0,0 +1,49 @@
+const getPresets = (isDev) => [
+  [
+    '@babel/preset-env',
+    {
+      targets: {
+        ie: '11',
+        edge: '17',
+        firefox: '60',
+        chrome: '67',
+        safari: '11.1',
+      },
+    },
+  ],
+  '@babel/preset-react',
+  '@babel/preset-typescript',
+  [
+    // Automatically includes the 'emotion' preset.
+    '@emotion/babel-preset-css-prop',
+    {
+      sourceMap: isDev,
+      autoLabel: isDev,
+      labelFormat: '[local]',
+      cssPropOptimization: !isDev,
+    },
+  ],
+];
+
+const BASE_PLUGINS = [
+  'macros',
+  '@babel/proposal-class-properties',
+  '@babel/proposal-object-rest-spread',
+  '@babel/proposal-private-methods',
+  '@babel/proposal-private-property-in-object',
+  '@babel/plugin-transform-runtime',
+  '@babel/plugin-proposal-optional-chaining',
+];
+
+module.exports = {
+  env: {
+    production: {
+      presets: getPresets(false),
+      plugins: BASE_PLUGINS,
+    },
+    development: {
+      presets: getPresets(true),
+      plugins: BASE_PLUGINS,
+    },
+  },
+};
